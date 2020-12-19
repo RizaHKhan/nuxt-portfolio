501e04151a549f0c14f20ab11a8463a6866dec6d
khanr <khanriza@gmail.com>
2020-12-19 14:28:06 -0500

    Dashboard Navigation bar with relevant components as links

---
+++ components/dashboard/Navigation.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app-bar app color="grey darken-4" flat>
+    <v-container class="py-0 fill-height">
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        text
+        @click="setActiveComponent(link)"
+      >
+        {{ link }}
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-responsive max-width="260">
+        <v-text-field
+          dense
+          flat
+          hide-details
+          rounded
+          solo-inverted
+        ></v-text-field>
+      </v-responsive>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+
+export default {
+  data: () => ({
+    links: ['User', 'History', 'Projects'],
+  }),
+  methods: {
+    ...mapMutations({
+      setActiveComponent: 'admin/setActiveComponent',
+    }),
+  },
+}
+</script>
