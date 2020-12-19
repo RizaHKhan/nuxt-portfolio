90c358e459c901a61ca500c135cc7fa915ff4800
khanr <khanriza@gmail.com>
2020-12-19 14:27:10 -0500

    New Dashboard layout

---
+++ layouts/dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app id="dark">
+    <Navigation />
+    <v-main class="grey darken-3">
+      <nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Navigation from '@/components/dashboard/Navigation'
+export default {
+  components: {
+    Navigation,
+  },
+}
+</script>
