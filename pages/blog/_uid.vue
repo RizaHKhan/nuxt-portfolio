72f1b429c6a0feb681bb7a11c367f51cc82a99d9
khanr <khanriza@gmail.com>
2021-02-08 17:30:38 -0500

    Changed name of folder to UID

---
+++ pages/blog/_uid.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-container>
+    <v-card class="ma-2" outlined>
+      <v-card-title class="font-weight-light">{{
+        blog.data.title[0].text
+      }}</v-card-title>
+      <v-card-subtitle>{{ blog.data.category.slug }}</v-card-subtitle>
+      <div v-for="(slice, x) in blog.data.body" :key="x">
+        <v-card-text v-if="slice.slice_type === 'text'">
+          <prismic-rich-text
+            class="textslice"
+            :field="slice.primary.text"
+          ></prismic-rich-text>
+        </v-card-text>
+        <prismic-image
+          v-if="slice.slice_type === 'image'"
+          height="300"
+          width="300"
+          :field="slice.primary.image"
+        />
+        <v-card-text
+          v-if="slice.slice_type === 'quote'"
+          class="text-h5 font-weight-light"
+        >
+          <blockquote>{{ $prismic.asText(slice.primary.quote) }}</blockquote>
+        </v-card-text>
+        <ImageGallery
+          v-if="slice.slice_type === 'image_gallery'"
+          height="300"
+          width="300"
+          :slice="slice"
+        />
+      </div>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import ImageGallery from '@/components/slices/ImageGallery'
+
+export default {
+  components: {
+    ImageGallery,
+  },
+  async asyncData({ $prismic, params }) {
+    console.log(params.uid)
+    try {
+      const blog = await $prismic.api.getByUID('blog', params.uid)
+      console.log('other')
+      console.log(blog)
+      return { blog }
+    } catch (e) {
+      console.log(e)
+      return { e }
+    }
+  },
+}
+</script>
