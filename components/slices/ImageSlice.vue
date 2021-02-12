f290d46bc34661ccb02a5e5b359a6a3e9ca86a12
khanr <khanriza@gmail.com>
2021-02-11 20:20:55 -0500

    added new components and transferred guts to them

---
+++ components/slices/ImageSlice.vue
@@ -0,0 +1,17 @@
+<template>
+  <prismic-image height="300" width="300" :field="slice.primary.image" />
+</template>
+
+<script>
+export default {
+  props: {
+    slice: {
+      type: Object,
+      required: true,
+      default: () => {},
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ components/slices/QuoteSlice.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card-text class="text-h5 font-weight-light">
+    <blockquote>{{ $prismic.asText(slice.primary.quote) }}</blockquote>
+  </v-card-text>
+</template>
+
+<script>
+export default {
+  props: {
+    slice: {
+      type: Object,
+      required: true,
+      default: () => {},
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
