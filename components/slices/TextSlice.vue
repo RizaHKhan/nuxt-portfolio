27bef7b7449ebcd97d39913cb97fea24d0abb0fe
khanr <khanriza@gmail.com>
2021-02-11 20:31:35 -0500

    added unique classes

--- components/slices/TextSlice.vue
+++ components/slices/TextSlice.vue
@@ -1,6 +1,9 @@
 <template>
   <v-card-text>
-    <prismic-rich-text :field="slice.primary.text"></prismic-rich-text>
+    <prismic-rich-text
+      :field="slice.primary.text"
+      :class="getClass(slice.primary.text[0].type)"
+    ></prismic-rich-text>
   </v-card-text>
 </template>
 
@@ -13,7 +16,35 @@ export default {
       default: () => {},
     },
   },
+  methods: {
+    getClass(type) {
+      switch (type) {
+        case 'preformatted':
+          return 'code'
+        case 'heading6':
+          return 'heading'
+        case 'heading2':
+          return 'heading'
+        case 'heading1':
+          return 'heading'
+        case 'paragraph':
+          return 'paragraph'
+      }
+    },
+  },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.code {
+  border: solid 1px red;
+}
+
+.heading {
+  border-bottom: 1px white solid;
+}
+
+.paragraph {
+  border: solid 1px blue;
+}
+</style>
